5327bb765e2f851b298be9ea8ab977536ca8e83d
Christian Zosel <christian@zosel.ch>
2022-03-17 23:21:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# highlightjs-jexl
+
+This adds support for [Jexl](https://github.com/TomFrost/Jexl) to [highlightjs](https://highlightjs.org/).
---
+++ src/index.js
@@ -0,0 +1,60 @@
+export default function(hljs) {
+  return {
+    name: 'Jexl',
+    contains: [
+      {
+        scope: 'string',
+        match: /(["'])(?:\\[\s\S]|(?!\1)[^\\])*\1/,
+        relevance: 0
+      },
+      // transforms
+      {
+        begin: [
+          /(\|\s*)/,
+          /[a-zA-Zа-яА-Я_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$][a-zA-Zа-яА-Я0-9_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$]*/,
+        ],
+        beginScope: {
+          1: "operator",
+          2: "title.function.call"
+        },
+      },
+      {
+        scope: 'title.function.call',
+        match: /[a-zA-Zа-яА-Я_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$][a-zA-Zа-яА-Я0-9_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$]*\s*(?=\()/,
+        relevance: 0
+      },
+      {
+        begin: [
+          /\.\s*/,
+          /[a-zA-Zа-яА-Я_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$][a-zA-Zа-яА-Я0-9_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$]*/,
+        ],
+        beginScope: {
+          1: "punctuation",
+          2: "property"
+        },
+      },
+      {
+        scope: 'number',
+        match: /\b\d+(?:\.\d+)?\b|\B\.\d+\b/,
+        relevance: 0
+      },
+      {
+        scope: 'operator',
+        match: /[<>!]=?|-|\+|&&|==|\|\|?|\/\/?|[?:*^%]/,
+        relevance: 0
+      },
+      { beginKeywords: [ "true", "false", "in"].join(" ") },
+      {
+        scope: 'punctuation',
+        match: /[{}[\](),.]/,
+        relevance: 0
+      },
+      {
+        scope: 'variable',
+        match: /[a-zA-Zа-яА-Я_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$][a-zA-Zа-яА-Я0-9_\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF$]*/,
+        relevance: 0
+      },
+    ],
+    illegal: '\\S'
+  };
+}
